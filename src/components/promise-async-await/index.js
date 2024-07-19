08b5ae9637e39631f946512d1aa732051f1f7d66
Sanjeev94 <sanjeev.dubey930@gmail.com>
2024-07-20 05:13:26 +0530

    prmise and async and await

--- src/components/promise-async-await/index.js
+++ src/components/promise-async-await/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 
 function PromiseAsyncAwait() {
+    // promise
     const promiseExample = () => {
         const friend = new Promise((resolve, reject) => {
             let isChocalteEveryday = false;
@@ -24,6 +25,58 @@ function PromiseAsyncAwait() {
     useEffect(() => {
         promiseExample();
     }, []);
+    // promise
+
+    const promiseOne = (paramOne) => {
+        return new Promise((resolve, reject) => {
+            if(paramOne === "Sanjeev") {
+                resolve("Promise Resolved");
+            }
+            else {
+                reject("Promise Rejected");
+            }
+        });
+    };
+    const promiseTwo = (paramTwo) => {
+        return new Promise((resolve, reject) => {
+            if(paramTwo === "Promise Resolved") {
+                resolve("Sanjeev good boy");
+            }
+            else {
+                reject("bad boy");
+            }
+        });
+    };
+   // promise
+    const PromiseMain = () => {
+        promiseOne("Sanjeev")
+        .then((fresponse) => {
+            console.log(fresponse);
+            return promiseTwo(fresponse);
+        })
+        .then((sresponse) => {
+            console.log(sresponse);
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+    };
+    // await
+    const asyncMain = async () => {
+       try {
+        let res = await promiseOne("Sanjeev");
+       console.log(res);
+       let resTwo = await promiseTwo(res);
+       console.log(resTwo);
+       }
+       catch{
+        console.log("promise rejected");
+       }
+    };
+    useEffect(() => {
+        asyncMain();
+    }, []);
+
   return (
     <div></div>
   )
